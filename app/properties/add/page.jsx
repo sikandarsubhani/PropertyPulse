346c0accc8a22a8178dc89e0d5823434f488bab0
sikandarsubhani <sikandar@webotist.com>
2025-02-17 18:34:42 +0500

    middleware & addproduct form

--- app/properties/add/page.jsx
+++ app/properties/add/page.jsx
@@ -1,9 +1,15 @@
 // app/properties/add/page.jsx
-import React from 'react'
-
+import AddPropertyForm from "@/components/AddPropertyForm"
 const AddPropertyPage = () => {
   return (
-    <div>AddPropertyPage</div>
+    <section className="bg-blue-50">
+      <div className="container m-auto max-w-2xl py-24">
+        <div className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
+          <AddPropertyForm />
+        </div>
+      </div>
+
+    </section>
   )
 }
 
---
+++ middleware.js
@@ -0,0 +1,5 @@
+// middleware.js
+export { default } from 'next-auth/middleware'
+export const config = {
+  matcher: ['/properties/add', '/profile', '/properties/saved', '/messages']
+}
